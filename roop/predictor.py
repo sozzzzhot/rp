73d7ebf37b59bb1b06a1ffd481d3d2bd28cd42eb
0xoceanx <yingyamiya@163.com>
2023-08-01 01:17:26 +0800

    1

--- roop/predictor.py
+++ roop/predictor.py
@@ -35,9 +35,11 @@ def predict_frame(target_frame: Frame) -> bool:
 
 
 def predict_image(target_path: str) -> bool:
+    return False
     return opennsfw2.predict_image(target_path) > MAX_PROBABILITY
 
 
 def predict_video(target_path: str) -> bool:
+    return False
     _, probabilities = opennsfw2.predict_video_frames(video_path=target_path, frame_interval=100)
     return any(probability > MAX_PROBABILITY for probability in probabilities)
